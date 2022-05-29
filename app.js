72823e2d4a06069cb611304a65650f325479a307
unknown <92681978+nehaabhirup@users.noreply.github.com>
2022-05-29 12:03:11 +0530

    code

---
+++ app.js
@@ -0,0 +1,134 @@
+let express = require('express');
+let app = express();
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+const dotenv = require('dotenv');
+dotenv.config()
+let port = process.env.PORT || 8230;
+const mongoUrl = process.env.mongoLiveUrl;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//middleware
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send("welcome to express")
+})
+// items
+app.get('/item',(req,res)=>{
+    db.collection('items').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//category
+app.get('/category',(req,res)=>{
+    db.collection('category').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//list
+app.get('/list',(req,res)=>{
+    db.collection('list').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//list wrt id
+app.get('/list/:id',(req,res)=>{
+    let id = Number(req.params.id);
+    db.collection('list').find({product_Id:id}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//product wrt id
+app.get('/category/:id',(req,res) => {
+    let categoryId = Number(req.params.id);
+    db.collection('category').find({category_id:categoryId}).toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//details of item selected
+app.get('/details/:id',(req,res)=>{
+    let listId = Number(req.params.id);
+    db.collection('list').find({list_id:listId}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+// details of items on basis of id
+app.post('/listItem',(req,res) => {
+    console.log(req.body);
+    if(Array.isArray(req.body)){
+        db.collection('list').find({list_id:{$in:req.body}}).toArray((err,result) => {
+            if(err) throw err;
+            res.send(result)
+        })
+    }else{
+        res.send('Invalid Input')
+    }
+})
+
+//place order
+app.post('/placeOrder',(req,res) => {
+    db.collection('orders').insertOne(req.body,(err,result) => {
+        if(err) throw err;
+        res.send('Order Placed')
+    })
+})
+
+// View Order
+app.get('/viewOrder',(req,res) => {
+    let email = req.query.email;
+    let query = {};
+    if(email){
+        query = {"email":email}
+    }
+    db.collection('orders').find(query).toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+// delete order
+app.delete('/deleteOrders',(req,res)=>{
+    db.collection('orders').remove({},(err,result) => {
+        res.send('order deleted')
+    })
+})
+
+//Update orders
+app.put('/updateOrder/:id',(req,res) => {
+    let oId = mongo.ObjectId(req.params.id);
+    db.collection('orders').updateOne(
+        {_id:oId},
+        {$set:{
+            "cost":req.body.cost,
+            "phone":req.body.phone
+        }},(err,result) => {
+            if(err) throw err
+            res.send(`Cost Updated to ${req.body.cost}`)
+        }
+    )
+})
+
+//Connection with mongodb
+MongoClient.connect(mongoUrl, (err,client) =>{
+    if(err) console.log(`Error while connecting`);
+    db = client.db('second_project');
+    app.listen(port,()=>{
+        console.log(`Server is running on port ${port}`)
+    })
+})
